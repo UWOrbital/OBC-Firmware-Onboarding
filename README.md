60b60133872dd5cdf97d45b68fbf883a0345512c
Eugene Hong <eugeneh1217@gmail.com>
2022-05-30 08:10:06 -0400

    Publish first attempt at debugging

--- README.md
+++ README.md
@@ -10,3 +10,12 @@ LED B: ||: o   x   |o   x   |o x o x |o x o x :||
 ...Where O represents an LED turning on, and X represents an LED turning off. Each LED is controlled in its own task.
 
 The errors are mostly classic C programming mistakes. If you don't have a background in operating systems, or freeRTOS specifically, you should still be able to find all the bugs with C knowledge and basic pattern recognition skills. Good luck!
+
+## Bugs Found:
+* add quotes to include on line 2: `#include sys_common.h` -> `#include "sys_common.h"`
+* add second task handle on line 20: `xTaskHandle xTask1Handle;` -> `xTaskHandle xTask1Handle, xTask2Handle;`
+* add semicolon on line 24: `BaseType_t retval1, retval2` -> `BaseType_t retval1, retval2;`
+* create second task after line 26: ` ` -> `retval2 = xTaskCreate( blinkGIOB2, BLINK2_NAME, configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, &xTask2Handle );`
+* adjust delay times on (line numbers are after fourth bug)
+    * lines 55/57: `vTaskDelay( BLINK2_DELAY_TICKS );` -> `vTaskDelay( BLINK2_DELAY_TICKS * 2 );`
+    * lines 62/64: `vTaskDelay( BLINK2_DELAY_TICKS * 2 );` -> `vTaskDelay( BLINK2_DELAY_TICKS );`
--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 /* Include Files */
-#include sys_common.h
+#include "sys_common.h"
 #include "gio.h"
 
 /* FreeRTOS Kernel includes. */
@@ -17,13 +17,14 @@
 #define BLINK2_PRIORITY 5
 #define BLINK2_DELAY_TICKS 200
 
-xTaskHandle xTask1Handle;
+xTaskHandle xTask1Handle, xTask2Handle;
 
 void main(void) {
     gioInit();
-    BaseType_t retval1, retval2
+    BaseType_t retval1, retval2;
 
     retval1 = xTaskCreate( blinkGIOB1, BLINK1_NAME, configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, &xTask1Handle );
+    retval2 = xTaskCreate( blinkGIOB2, BLINK2_NAME, configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, &xTask2Handle );
 
     if(retval1 && retval2){
         vTaskStartScheduler();
@@ -51,16 +52,16 @@ void blinkGIOB2( void * arg )
         for (int i = 0; i < 2; i++)
         {
             gioSetBit(gioPORTB, 2, TRUE);
-            vTaskDelay( BLINK2_DELAY_TICKS);
+            vTaskDelay( BLINK2_DELAY_TICKS * 2 );
             gioSetBit(gioPORTB, 2, FALSE);
-            vTaskDelay( BLINK2_DELAY_TICKS);
+            vTaskDelay( BLINK2_DELAY_TICKS * 2 );
         }
         for (int i = 0; i < 4; i++)
         {
             gioSetBit(gioPORTB, 2, TRUE);
-            vTaskDelay( BLINK2_DELAY_TICKS * 2);
+            vTaskDelay( BLINK2_DELAY_TICKS );
             gioSetBit(gioPORTB, 2, FALSE);
-            vTaskDelay( BLINK2_DELAY_TICKS * 2);
+            vTaskDelay( BLINK2_DELAY_TICKS );
         }
     }
 }
