33acc9e397bd3f0735b2a0190c04b3560a7ef42b
Nathan Tang <nathantangca@outlook.com>
2022-09-10 17:38:54 -0400

    Partial work on Light Service

--- onboarding/source/amb_light_service.c
+++ onboarding/source/amb_light_service.c
@@ -3,9 +3,15 @@
 
 #include <adc.h>
 #include <sci.h>
+#include <FreeRTOS.h>
+#include <os_task.h>
+#include <os_queue.h>
 
 /* USER CODE BEGIN */
 // Include any additional headers and global variables here
+static TaskHandle_t lightServiceTaskHandle = NULL;
+static QueueHandle_t xQueue1 = NULL; 
+
 
 /* USER CODE END */
 
@@ -18,7 +24,18 @@ static void lightServiceTask(void * pvParameters);
 uint8_t initLightService(void) {
     /* USER CODE BEGIN */
     // Create the task and queue here.
-
+    BaseType_t xReturned = pdFAIL;
+    if (lightServiceTaskHandle = NULL) {
+        xReturned = xTaskCreate(lightServiceTask,
+                                LIGHT_SERVICE_NAME,
+                                LIGHT_SERVICE_STACK_SIZE,
+                                (void *) 0,
+                                LIGHT_SERVICE_PRIORITY,
+                                &lightServiceTaskHandle);
+    if (xQueue1 = NULL) {
+        xQueue1 = xQueueCreate(QUEUE_LENGTH, QUEUE_ITEM_SIZE);
+    }
+    }
     /* USER CODE END */
     return 1;
 }
@@ -26,6 +43,14 @@ uint8_t initLightService(void) {
 static void lightServiceTask(void * pvParameters) {
     /* USER CODE BEGIN */
     // Wait for MEASURE_LIGHT event in the queue and then print the ambient light value to the serial port.
+    ASSERT(lightServiceTaskHandle != NULL);
+
+    light_event_t *event;
+    while(1) {
+        if (xQueueReceive(xQueue1, (void *) event, (TickType_t) 0) == pdPASS) {
+            
+        }
+    }
 
     /* USER CODE END */
 }
@@ -33,7 +58,12 @@ static void lightServiceTask(void * pvParameters) {
 uint8_t sendToLightServiceQueue(light_event_t *event) {
     /* USER CODE BEGIN */
     // Send the event to the queue.
-    
+    if (xQueueSend(xQueue1, (void *) event, (TickType_t) 0) == pdPASS) {
+        return 1;
+    }
     /* USER CODE END */
     return 0;
 }
+
+// Function that does ADC conversion and returns ambient light value
+uint32_t getAmbientLightData(void);
