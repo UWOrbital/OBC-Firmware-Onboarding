04f81a731ed32117191a43385ffc95aade6e0359
Nathan Tang <nathantangca@outlook.com>
2022-09-10 11:25:35 -0400

    Controller Completed

--- onboarding/source/controller.c
+++ onboarding/source/controller.c
@@ -56,7 +56,13 @@ uint8_t initController(void) {
 
     /* USER CODE BEGIN */
     // Create light timer and check if task/timers were created successfully
-
+    if (lightTimerHandle = NULL) {
+        lightTimerHandle = xTimerCreate(LIGHT_TIMER_NAME,
+                                        LIGHT_TIMER_PERIOD,
+                                        LIGHT_TIMER_AUTORELOAD,
+                                        (void *) 0,
+                                        lightTimerCallback);
+    }
     /* USER CODE END */
 
     return 1;
@@ -70,7 +76,8 @@ static void controllerTask(void * pvParameters) {
     if (lightServiceStatus == 0) {
         /* USER CODE BEGIN */
         // Deal with error when initializing light service task and/or queue
-
+        unsigned char errorText = "ERRROR INITIALIZING TASK";
+        sciPrintText(scilinREG, errorText, strlen(errorText));
         /* USER CODE END */
     } else { 
         /* Light service task and queue created successfully */
@@ -78,7 +85,7 @@ static void controllerTask(void * pvParameters) {
         
         /* USER CODE BEGIN */
         // Start light timer
-
+        xReturned = xTimerStart(lightTimerHandle, 0);
         /* USER CODE END */
     }
 
@@ -94,6 +101,9 @@ static void ledTimerCallback(TimerHandle_t xTimer) {
 static void lightTimerCallback(TimerHandle_t xTimer) {
     /* USER CODE BEGIN */
     // Send light event to light service queue
+    ASSERT(xTimer != NULL);
+
+    sendToLightServiceQueue(MEASURE_LIGHT);
 
     /* USER CODE END */
 }
