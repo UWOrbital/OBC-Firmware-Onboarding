7a0f9bc594f30784786ff4599438af7966e5c99e
caiicar293 <124074010+caiicar293@users.noreply.github.com>
2023-02-17 11:38:14 -0500

    Update controller.h

--- onboarding/include/controller.h
+++ onboarding/include/controller.h
@@ -1,13 +1,32 @@
 #ifndef ONBOARDING_INCLUDE_CONTROLLER_H_
 #define ONBOARDING_INCLUDE_CONTROLLER_H_
 
-#include "obc_errors.h"
+#include <os_projdefs.h>
+#include <stdint.h>
+
+/* Controller task config */
+#define CONTROLLER_NAME         "controller"
+#define CONTROLLER_STACK_SIZE   256
+#define CONTROLLER_PRIORITY     1
+
+/* LED timer config */
+#define LED_TIMER_NAME          "led_timer"
+#define LED_TIMER_PERIOD        pdMS_TO_TICKS(2500)
+#define LED_TIMER_AUTORELOAD    pdTRUE
+
+/* USER CODE BEGIN */
+/* Light controller config */
+#define LIGHT_CONTROLLER_NAME         "light timer" 
+#define LIGHT_CONTROLLER_PERIOD        pdMS_TO_TICKS(1000)
+#define LIGHT_CONTROLLER_AUTORELOAD    pdTRUE
+#define ERROR_MESSAGE                 "Error" 
+/* USER CODE END */
 
 /**
  * @brief Initialize the controller task and any timers.
  * 
- * @return obc_error_code_t OBC_ERR_CODE_SUCCESS if successful, otherwise an error code. 
+ * @return uint8_t 1 if successful, 0 otherwise 
  */
-obc_error_code_t initController(void);
+uint8_t initController(void);
 
 #endif /* ONBOARDING_INCLUDE_CONTROLLER_H_ */
