c20907191a19a2c4aac42086449673b53ec36888
Jerry Hu <j376hu@uwaterloo.ca>
2023-03-09 20:21:01 -0500

    Fix helper function bugs

--- onboarding/source/amb_light_service.c
+++ onboarding/source/amb_light_service.c
@@ -33,7 +33,7 @@ static StackType_t lightServiceTaskStack[LIGHT_SERVICE_STACK_SIZE];
 uint8_t lightServiceQueueStorage[ QUEUE_LENGTH * ITEM_SIZE ];
 static StaticQueue_t lightServiceQueue;
 static QueueHandle_t lightServiceQueueHandle = NULL;
-void adcReadandPrintData(unsigned int regNum, unsigned int groupNum);
+void adcReadandPrintData(adcBASE_t *regNum, uint32 groupNum);
 /* USER CODE END */
 
 /**
@@ -102,7 +102,7 @@ static void lightServiceTask(void * pvParameters) {
 
 /* USER CODE BEGIN */
 // Read and print the register value to the serial port
-void adcReadandPrintData(unsigned int regNum, unsigned int groupNum) {
+void adcReadandPrintData(adcBASE_t *regNum, uint32 groupNum) {
     adcStartConversion(regNum, groupNum);
     if (adcIsConversionComplete(regNum, groupNum) == ADC_CONVERSION_IS_FINISHED) 
     {
