101f229abed91d91fa343947c1447ec4de9d4d15
fayzull0h <fayzulloh2004@gmail.com>
2023-05-10 14:40:48 -0400

    Removed unnecessary include

--- challenge.c
+++ challenge.c
@@ -2,7 +2,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <inttypes.h>
+//#include <inttypes.h>
 
 //-------------------------------------------------------------------------
 // Question 0
