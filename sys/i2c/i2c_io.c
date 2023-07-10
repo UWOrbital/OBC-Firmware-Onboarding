072c93ba47d34835b87a7b482a41f5af3060fd0e
George Jiang <gjjiang@uwaterloo.ca>
2023-07-10 00:40:21 -0400

    Added register checks for send and receive

--- sys/i2c/i2c_io.c
+++ sys/i2c/i2c_io.c
@@ -1,5 +1,6 @@
 #include "i2c_io.h"
 #include "errors.h"
+#include "lm75bd.h"
 
 #include <FreeRTOS.h>
 #include <os_portmacro.h>
@@ -12,7 +13,10 @@
 
 // The mutex timeout for the I2C bus
 #define I2C_MUTEX_TIMEOUT pdMS_TO_TICKS(500)
-#define LM75BD_I2C_ADDR 0x4FU
+
+#define temp_reg 0U
+#define config_reg 1U
+
 static SemaphoreHandle_t i2cMutex;
 static StaticSemaphore_t i2cMutexBuffer;
 
@@ -49,10 +53,24 @@ error_code_t i2cSendTo(uint8_t sAddr, uint8_t *buf, uint16_t numBytes) {
   }
 
   /* Mock the transmit */
-  if(sAddr == LM75BD_I2C_ADDR){
+  if(sAddr == LM75BD_OBC_I2C_ADDR){
+    uint8_t reg = buf[0];
+    switch (reg) {
+    case temp_reg:
+      // To Do
+      break;
+    case config_reg:
+      // To Do
+      break;
+    default:
+      xSemaphoreGive(i2cMutex);
+      printConsole("Unknown register sent\n");
+      return ERR_CODE_LM75BD_INVALID_REG;
+      break;
+    }
     memcpy(&I2CSentData, buf, sizeof(I2CSentData));
     if(numBytes>sizeof(I2CSentData)){
-      // To Do
+      printConsole("Buffer too large!\n");
     }
   } else {
     xSemaphoreGive(i2cMutex);
@@ -72,15 +90,28 @@ error_code_t i2cReceiveFrom(uint8_t sAddr, uint8_t *buf, uint16_t numBytes) {
   }
 
   /* Mock the receive using getLm75bdNextTempRegVal() */
-  if(sAddr == LM75BD_I2C_ADDR){
-    uint16_t recv_buff = getLm75bdNextTempRegVal();
+  if(sAddr == LM75BD_OBC_I2C_ADDR){
+    uint8_t reg = I2CSentData >> 8;
+    uint16_t recv_buff;
+    
+    switch(reg){
+    case temp_reg:
+      recv_buff = getLm75bdNextTempRegVal();
+      break;
+    case config_reg:
+      // do something
+      break;
+    default:
+      recv_buff = 0U;
+    }
+
     memset(buf, 0, numBytes);
     if(numBytes <= sizeof(recv_buff)){
       memcpy(buf, &recv_buff, numBytes);
     } 
     else {
       memcpy(buf, &recv_buff, sizeof(recv_buff));
-      // exit or throw error instead?
+      printConsole("Buffer too large!\n");
     }
   } else {
     xSemaphoreGive(i2cMutex);
