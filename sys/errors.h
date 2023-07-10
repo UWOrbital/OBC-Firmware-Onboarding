84a82d90aa94ef8c25dbe792258cedfb5ddfe72c
George Jiang <gjjiang@uwaterloo.ca>
2023-07-09 22:52:51 -0400

    Added mock functionality for recv and send. Added i2c timeout error

--- sys/errors.h
+++ sys/errors.h
@@ -8,7 +8,7 @@ typedef enum {
     ERR_CODE_UNKNOWN = 1,
     ERR_CODE_INVALID_ARG = 2,
     ERR_CODE_INVALID_STATE = 3,
-    
+    ERR_CODE_I2C_TRANSFER_TIMEOUT = 4,
     /* FreeRTOS errors */
     ERR_CODE_MUTEX_TIMEOUT = 100,
     ERR_CODE_QUEUE_FULL = 101,
--- sys/i2c/i2c_io.c
+++ sys/i2c/i2c_io.c
@@ -12,12 +12,13 @@
 
 // The mutex timeout for the I2C bus
 #define I2C_MUTEX_TIMEOUT pdMS_TO_TICKS(500)
-
+#define LM75BD_I2C_ADDR 0x4FU
 static SemaphoreHandle_t i2cMutex;
 static StaticSemaphore_t i2cMutexBuffer;
 
 // For automated testing
 static uint16_t lm75bdNextTempRegVal;
+static uint16_t I2CSentData;
 
 void setLm75bdNextTempRegVal(uint16_t val) {
   // This function is only called from the test suite (controller task)
@@ -48,7 +49,15 @@ error_code_t i2cSendTo(uint8_t sAddr, uint8_t *buf, uint16_t numBytes) {
   }
 
   /* Mock the transmit */
-
+  if(sAddr == LM75BD_I2C_ADDR){
+    memcpy(&I2CSentData, buf, sizeof(I2CSentData));
+    if(numBytes>sizeof(I2CSentData)){
+      // To Do
+    }
+  } else {
+    xSemaphoreGive(i2cMutex);
+    return ERR_CODE_I2C_TRANSFER_TIMEOUT;
+  }
   xSemaphoreGive(i2cMutex);  // Won't fail because the mutex is taken correctly
   return ERR_CODE_SUCCESS;
 }
@@ -63,7 +72,20 @@ error_code_t i2cReceiveFrom(uint8_t sAddr, uint8_t *buf, uint16_t numBytes) {
   }
 
   /* Mock the receive using getLm75bdNextTempRegVal() */
-
+  if(sAddr == LM75BD_I2C_ADDR){
+    uint16_t recv_buff = getLm75bdNextTempRegVal();
+    memset(buf, 0, numBytes);
+    if(numBytes <= sizeof(recv_buff)){
+      memcpy(buf, &recv_buff, numBytes);
+    } 
+    else {
+      memcpy(buf, &recv_buff, sizeof(recv_buff));
+      // exit or throw error instead?
+    }
+  } else {
+    xSemaphoreGive(i2cMutex);
+    return ERR_CODE_I2C_TRANSFER_TIMEOUT;
+  }
   xSemaphoreGive(i2cMutex);  // Won't fail because the mutex is taken correctly
   return ERR_CODE_SUCCESS;
 }
