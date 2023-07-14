3daf94a585ee799da98d48c49e9c6c41b05fc144
Ali Taha <ataha.blackberry.com>
2023-07-14 15:47:21 -0400

    Final

--- challenge.c
+++ challenge.c
@@ -212,10 +212,8 @@ void *q13(uint32_t *ptr1, uint16_t *ptr2)
         return (void *)-1;
     }
 
-    uintptr_t minAddress = (uintptr_t)ptr1 < (uintptr_t)ptr2 ? (uintptr_t)ptr1 : (uintptr_t)ptr2;
-    minAddress += 5 * sizeof(uint16_t);
-
-    return (void *)minAddress;
+    void *minAddress = MIN((void *)ptr1, (void *)ptr2);
+    return minAddress + 5;
 }
 //-------------------------------------------------------------------------
 // The following function is used to test your code. Do not remove any
