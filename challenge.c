7fe926daab81a83f14aeeae5089c9535a48a4b7d
Ali Taha <ataha.blackberry.com>
2023-07-14 15:36:34 -0400

    Final

--- challenge.c
+++ challenge.c
@@ -212,7 +212,7 @@ void *q13(uint32_t *ptr1, uint16_t *ptr2)
         return (void *)-1;
     }
 
-    uint32_t *minAdress = ((uintptr_t)ptr1 < (uintptr_t)ptr2 ? (uintptr_t)ptr1 : (uintptr_t)ptr2);
+    uint32_t *minAdress = (uint32_t *)((uintptr_t)ptr1 < (uintptr_t)ptr2 ? (uintptr_t)ptr1 : (uintptr_t)ptr2);
     minAdress += 5 * sizeof(uint32_t);
 
     return (void *)minAdress;
