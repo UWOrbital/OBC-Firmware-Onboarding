61167e1fc1d2858f7987d8c5724b8490c12b0b27
Ali Taha <aliestaha04@gmail.com>
2023-08-15 18:18:38 -0400

    I have no clue how to convert the error code to string i tried everything

--- services/thermal_mgr/thermal_mgr.c
+++ services/thermal_mgr/thermal_mgr.c
@@ -87,11 +87,8 @@ static void thermalMgr(void *pvParameters)
         checker = readTempLM75BD(LM75BD_OBC_I2C_ADDR, &currTemp);
         if (checker != ERR_CODE_SUCCESS)
         {
-          // Convert the error code to a string using to_string
-          string errorString = to_string(static_cast<int>(checker));
-
           // Print the error string to the console
-          printConsole("Error code: %s", errorString.c_str());
+          printConsole("Error");
           continue;
         }
         addTemperatureTelemetry(currTemp);
@@ -103,7 +100,7 @@ static void thermalMgr(void *pvParameters)
           // Convert the error code to a string using to_string
           string errorString = to_string(static_cast<int>(checker));
           // Print the error string to the console
-          printConsole("Error code: %s", errorString.c_str());
+          printConsole("Error");
           continue;
         }
         if (currTemp >= 80)
