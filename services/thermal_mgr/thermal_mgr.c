3f58815c47895c77fd490bea048d05b8863fdea2
Guhan Iyer <guhan.shyam.iyer05@gmail.com>
2023-09-07 23:35:34 -0400

    Completed but untested onboarding

--- services/thermal_mgr/thermal_mgr.c
+++ services/thermal_mgr/thermal_mgr.c
@@ -24,48 +24,110 @@ static uint8_t thermalMgrQueueStorageArea[THERMAL_MGR_QUEUE_LENGTH * THERMAL_MGR
 
 static void thermalMgr(void *pvParameters);
 
-void initThermalSystemManager(lm75bd_config_t *config) {
+void initThermalSystemManager(lm75bd_config_t *config)
+{
   memset(&thermalMgrTaskBuffer, 0, sizeof(thermalMgrTaskBuffer));
   memset(thermalMgrTaskStack, 0, sizeof(thermalMgrTaskStack));
-  
+
   thermalMgrTaskHandle = xTaskCreateStatic(
-    thermalMgr, "thermalMgr", THERMAL_MGR_STACK_SIZE,
-    config, 1, thermalMgrTaskStack, &thermalMgrTaskBuffer);
+      thermalMgr, "thermalMgr", THERMAL_MGR_STACK_SIZE,
+      config, 1, thermalMgrTaskStack, &thermalMgrTaskBuffer);
 
   memset(&thermalMgrQueueBuffer, 0, sizeof(thermalMgrQueueBuffer));
   memset(thermalMgrQueueStorageArea, 0, sizeof(thermalMgrQueueStorageArea));
 
   thermalMgrQueueHandle = xQueueCreateStatic(
-    THERMAL_MGR_QUEUE_LENGTH, THERMAL_MGR_QUEUE_ITEM_SIZE,
-    thermalMgrQueueStorageArea, &thermalMgrQueueBuffer);
-
+      THERMAL_MGR_QUEUE_LENGTH, THERMAL_MGR_QUEUE_ITEM_SIZE,
+      thermalMgrQueueStorageArea, &thermalMgrQueueBuffer);
 }
 
-error_code_t thermalMgrSendEvent(thermal_mgr_event_t *event) {
-  /* Send an event to the thermal manager queue */
+error_code_t thermalMgrSendEvent(thermal_mgr_event_t *event)
+{
+  /* Returns appropriate error code according to queue status*/
+  if (event == NULL)
+  {
+    return ERR_CODE_INVALID_ARG;
+  }
+  if (xQueueSend(thermalMgrQueueHandle, (void *)event, portMAX_DELAY) == errQUEUE_FULL)
+  {
+    return ERR_CODE_QUEUE_FULL;
+  }
 
   return ERR_CODE_SUCCESS;
 }
 
-void osHandlerLM75BD(void) {
+void osHandlerLM75BD(void)
+{
   /* Implement this function */
+
+  /* Declare event of interrupt type and send to the queue*/
+  thermal_mgr_event_t interrupt;
+  interrupt.type = THERMAL_MGR_EVENT_INTERRUPT;
+  thermalMgrSendEvent(&interrupt);
 }
 
-static void thermalMgr(void *pvParameters) {
+static void thermalMgr(void *pvParameters)
+{
   /* Implement this task */
-  while (1) {
-    
+  while (1)
+  {
+    /* Variable declarations*/
+    thermal_mgr_event_t currentItem;
+    error_code_t status;
+    float currentTemp = 0;
+
+    /* Current item is only processed if it was successfully recieved from the queue*/
+    if (xQueueReceive(thermalMgrQueueHandle, &currentItem, portMAX_DELAY) == pdTRUE)
+    {
+      /* Switch statement to perform type comparison on current item*/
+      switch (currentItem.type)
+      {
+      /* In the case of an item of normal/default type, record the current temperature and, if successful, print current temp to console*/
+      case THERMAL_MGR_EVENT_MEASURE_TEMP_CMD:
+        status = readTempLM75BD(LM75BD_OBC_I2C_ADDR, &currentTemp);
+        if (status != ERR_CODE_SUCCESS)
+        {
+          printConsole(status);
+          continue;
+        }
+        addTemperatureTelemetry(currentTemp);
+
+      /* If item is an OS interrupt, check if the current temperature is past hysteresis conditions and call the appropriate function*/
+      case THERMAL_MGR_EVENT_INTERRUPT:
+        status = readTempLM75BD(LM75BD_OBC_I2C_ADDR, &currentTemp);
+        if (status != ERR_CODE_SUCCESS)
+        {
+          printConsole(status);
+          continue;
+        }
+
+        if (currentTemp > 75)
+        {
+          overTemperatureDetected();
+        }
+        else
+        {
+          safeOperatingConditions();
+        }
+        break;
+      default:
+        break;
+      }
+    }
   }
 }
 
-void addTemperatureTelemetry(float tempC) {
+void addTemperatureTelemetry(float tempC)
+{
   printConsole("Temperature telemetry: %f deg C\n", tempC);
 }
 
-void overTemperatureDetected(void) {
+void overTemperatureDetected(void)
+{
   printConsole("Over temperature detected!\n");
 }
 
-void safeOperatingConditions(void) { 
+void safeOperatingConditions(void)
+{
   printConsole("Returned to safe operating conditions!\n");
 }
--- sys/i2c/i2c_io.c
+++ sys/i2c/i2c_io.c
@@ -23,83 +23,99 @@ static uint16_t lm75bdNextTempRegVal = 0;
 static uint8_t lastTxBuff[2] = {0};
 static uint8_t isOsActive = 0;
 
-void initI2C(void) {
+void initI2C(void)
+{
   memset(&i2cMutexBuffer, 0, sizeof(i2cMutexBuffer));
   i2cMutex = xSemaphoreCreateMutexStatic(&i2cMutexBuffer);
 }
 
 /* MOCKED I2C Functions. DO NOT MODIFY */
 
-error_code_t i2cSendTo(uint8_t sAddr, uint8_t *buf, uint16_t numBytes) {
-  if (buf == NULL || numBytes < 1) return ERR_CODE_INVALID_ARG;
+error_code_t i2cSendTo(uint8_t sAddr, uint8_t *buf, uint16_t numBytes)
+{
+  if (buf == NULL || numBytes < 1)
+    return ERR_CODE_INVALID_ARG;
 
-  if (i2cMutex == NULL) return ERR_CODE_INVALID_STATE;
+  if (i2cMutex == NULL)
+    return ERR_CODE_INVALID_STATE;
 
-  if (xSemaphoreTake(i2cMutex, I2C_MUTEX_TIMEOUT) != pdTRUE) {
+  if (xSemaphoreTake(i2cMutex, I2C_MUTEX_TIMEOUT) != pdTRUE)
+  {
     return ERR_CODE_MUTEX_TIMEOUT;
   }
 
   lastTxBuff[0] = buf[0];
 
-  if (numBytes > 1) {
+  if (numBytes > 1)
+  {
     lastTxBuff[1] = buf[1];
   }
 
-  xSemaphoreGive(i2cMutex);  // Won't fail because the mutex is taken correctly
+  xSemaphoreGive(i2cMutex); // Won't fail because the mutex is taken correctly
 
   return ERR_CODE_SUCCESS;
 }
 
-error_code_t i2cReceiveFrom(uint8_t sAddr, uint8_t *buf, uint16_t numBytes) {
-  if (buf == NULL || numBytes < 1) return ERR_CODE_INVALID_ARG;
+error_code_t i2cReceiveFrom(uint8_t sAddr, uint8_t *buf, uint16_t numBytes)
+{
+  if (buf == NULL || numBytes < 1)
+    return ERR_CODE_INVALID_ARG;
 
-  if (i2cMutex == NULL) return ERR_CODE_INVALID_STATE;
+  if (i2cMutex == NULL)
+    return ERR_CODE_INVALID_STATE;
 
-  if (xSemaphoreTake(i2cMutex, I2C_MUTEX_TIMEOUT) != pdTRUE) {
+  if (xSemaphoreTake(i2cMutex, I2C_MUTEX_TIMEOUT) != pdTRUE)
+  {
     return ERR_CODE_MUTEX_TIMEOUT;
   }
 
   uint16_t nextTemp = getLm75bdNextTempRegVal();
 
-  switch (lastTxBuff[0]) {
-    case 0:
-      buf[0] = (nextTemp >> 8) & 0xFF;
-      buf[1] = nextTemp & 0xFF;
-      break;
-    default:
-      for (int i = 0; i < numBytes; i++) {
-        buf[i] = 0;
-      }
+  switch (lastTxBuff[0])
+  {
+  case 0:
+    buf[0] = (nextTemp >> 8) & 0xFF;
+    buf[1] = nextTemp & 0xFF;
+    break;
+  default:
+    for (int i = 0; i < numBytes; i++)
+    {
+      buf[i] = 0;
+    }
   }
 
   setOsActive(0);
 
-  xSemaphoreGive(i2cMutex);  // Won't fail because the mutex is taken correctly
+  xSemaphoreGive(i2cMutex); // Won't fail because the mutex is taken correctly
   return ERR_CODE_SUCCESS;
 }
 
 /* TEST ENVIRONMENT FUNCTIONS */
-void setOsActive(uint8_t val) {
+void setOsActive(uint8_t val)
+{
   portENTER_CRITICAL();
   isOsActive = val;
   portEXIT_CRITICAL();
 }
 
-uint8_t getOsActive(void) {
+uint8_t getOsActive(void)
+{
   portENTER_CRITICAL();
   uint8_t isActive = isOsActive;
   portENTER_CRITICAL();
   return isActive;
 }
 
-void setLm75bdNextTempRegVal(uint16_t val) {
+void setLm75bdNextTempRegVal(uint16_t val)
+{
   // This function is only called from the test suite (controller task)
   portENTER_CRITICAL();
   lm75bdNextTempRegVal = val;
   portEXIT_CRITICAL();
 }
 
-uint16_t getLm75bdNextTempRegVal(void) {
+uint16_t getLm75bdNextTempRegVal(void)
+{
   portENTER_CRITICAL();
   uint16_t val = lm75bdNextTempRegVal;
   portEXIT_CRITICAL();
