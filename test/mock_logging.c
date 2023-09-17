e58dbb83bc7d69d99cc1e3ce177d2d8c3fb7ed48
Navtaj Hundal <n2hundal@uwaterloo.ca>
2023-09-17 14:12:16 -0400

    .

--- test/mock_logging.c
+++ test/mock_logging.c
@@ -5,7 +5,7 @@
 #include <stdint.h>
 #include <stdio.h>
 
-void logSetLevel(log_level_t newLogLevel) { logLevel = newLogLevel; }
+void logSetLevel(log_level_t newLogLevel) { /* do nothing */ }
 
 error_code_t logLog(log_level_t msgLevel, const char *file, uint32_t line, const char *s, ...) {
   return ERR_CODE_SUCCESS;
