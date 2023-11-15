554d183c7935599bbf52479aae936da470032de9
Leo Qi <leo@leozqi.com>
2023-11-15 00:06:12 -0500

    Completed onboarding challenge
    - Passed temperature sensor unit tests
    - Integration tests: Over temperature detected entering 82 degC;
      safe-temperature resumption detected at 74 degC.
    
    Implemented:
    
    - lm75bd.c readTempLM75BD function
    - thermal_mgr.c thermalMgr, osHandlerLM75BD functions

--- lm75bd/lm75bd.c
+++ lm75bd/lm75bd.c
@@ -9,6 +9,7 @@
 
 /* LM75BD Registers (p.8) */
 #define LM75BD_REG_CONF 0x01U  /* Configuration Register (R/W) */
+#define LM75BD_REG_TEMP 0x00U  /* Temperature Register (RO) */
 
 error_code_t lm75bdInit(lm75bd_config_t *config) {
   error_code_t errCode;
@@ -25,9 +26,43 @@ error_code_t lm75bdInit(lm75bd_config_t *config) {
   return ERR_CODE_SUCCESS;
 }
 
+// 2 Bytes of Data
+#define CONF_POINTER_BUFF_SIZE 1U
+#define CONF_READ_BUFF_SIZE 2U
+#define MASK_SIGN_BIT 0x7FU
+#define MASK_NINE_BIT 0x1FFU
+/**
+ * @brief Reads LM75BD temperature using I2C
+ */
 error_code_t readTempLM75BD(uint8_t devAddr, float *temp) {
   /* Implement this driver function */
-  
+  error_code_t errCode;
+
+  /* Write pointer register */
+  uint8_t point_buff[CONF_POINTER_BUFF_SIZE] = {0};
+  point_buff[0] = 0x00U;
+ 
+  errCode = i2cSendTo(LM75BD_OBC_I2C_ADDR, point_buff, CONF_POINTER_BUFF_SIZE);
+  if (errCode != ERR_CODE_SUCCESS)
+    return errCode;
+
+  /* Read temperature register */
+  uint8_t read_buff[CONF_READ_BUFF_SIZE] = {0};
+  errCode = i2cReceiveFrom(LM75BD_OBC_I2C_ADDR, read_buff, CONF_READ_BUFF_SIZE);
+  if (errCode != ERR_CODE_SUCCESS)
+    return errCode;
+
+  // Concatenate D9 through D0; read data sheet sect 7.4.3
+  // <https://www.nxp.com/docs/en/data-sheet/LM75B.pdf>
+  uint16_t temp_data = ((read_buff[0] & MASK_SIGN_BIT) << 3) | (read_buff[1] >> 5);
+  if ((read_buff[0] >> 7) & 0x01) {
+    // D10 1; temperature negative .. convert from two's complement
+    (*temp) = ((float) (~temp_data & MASK_NINE_BIT)+1) * -0.125;
+  } else {
+    (*temp) = ((float) temp_data) * 0.125; 
+  }
+    // D10 0; temperature positive
+
   return ERR_CODE_SUCCESS;
 }
 
--- services/thermal_mgr/thermal_mgr.c
+++ services/thermal_mgr/thermal_mgr.c
@@ -41,20 +41,57 @@ void initThermalSystemManager(lm75bd_config_t *config) {
 
 }
 
+#define MAX_MS_WAIT 20
 error_code_t thermalMgrSendEvent(thermal_mgr_event_t *event) {
   /* Send an event to the thermal manager queue */
+  xQueueSend(thermalMgrQueueHandle, (void *)event, portTICK_PERIOD_MS*MAX_MS_WAIT);
+  return ERR_CODE_SUCCESS;
+}
 
+error_code_t thermalMgrSendEventISR(thermal_mgr_event_t *event) {
+  /* Send an event to the thermal manager queue */
+  xQueueSendFromISR(thermalMgrQueueHandle, (void *)event, NULL);
   return ERR_CODE_SUCCESS;
 }
 
 void osHandlerLM75BD(void) {
   /* Implement this function */
+  // Use new thermalMgr event for non-blocking.
+  thermal_mgr_event_t evt = {0};
+  evt.type = THERMAL_MGR_EVENT_OVER_TEMP_CMD;
+  thermalMgrSendEventISR(&evt);
 }
 
 static void thermalMgr(void *pvParameters) {
   /* Implement this task */
+  lm75bd_config_t config = *(lm75bd_config_t *) pvParameters;
+  float temp = {0};
+  error_code_t err_code = {0};
+
   while (1) {
-    
+    thermal_mgr_event_t temp_evt = {0};
+    xQueueReceive(thermalMgrQueueHandle, (void *)&temp_evt, portTICK_PERIOD_MS*MAX_MS_WAIT);
+
+    switch (temp_evt.type) {
+    case THERMAL_MGR_EVENT_MEASURE_TEMP_CMD:
+      /* Measure temperature */
+      err_code = readTempLM75BD(config.devAddr, &temp);
+      if (err_code == ERR_CODE_SUCCESS) {
+        addTemperatureTelemetry(temp); // ambiguous error case
+      }
+      break;
+    case THERMAL_MGR_EVENT_OVER_TEMP_CMD:
+      err_code = readTempLM75BD(config.devAddr, &temp);
+      if (err_code == ERR_CODE_SUCCESS) {
+        if (temp <= config.hysteresisThresholdCelsius) {
+          // safe temperature
+          safeOperatingConditions();
+        } else {
+          overTemperatureDetected();
+        }
+      }
+      break;
+    }
   }
 }
 
