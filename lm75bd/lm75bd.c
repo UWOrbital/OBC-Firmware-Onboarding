a06981ada7624c9ff138380c92d46ffea691cc42
Brian Zhang <brian.w.zhang@gmail.com>
2024-01-08 19:46:49 -0500

    Temperature Sensor Driver Functions done

--- lm75bd/lm75bd.c
+++ lm75bd/lm75bd.c
@@ -8,12 +8,15 @@
 #include <math.h>
 
 /* LM75BD Registers (p.8) */
-#define LM75BD_REG_CONF 0x01U  /* Configuration Register (R/W) */
+#define LM75BD_REG_CONF 0x01U /* Configuration Register (R/W) */
+#define TEMP_REG 0x00
 
-error_code_t lm75bdInit(lm75bd_config_t *config) {
+error_code_t lm75bdInit(lm75bd_config_t *config)
+{
   error_code_t errCode;
 
-  if (config == NULL) return ERR_CODE_INVALID_ARG;
+  if (config == NULL)
+    return ERR_CODE_INVALID_ARG;
 
   RETURN_IF_ERROR_CODE(writeConfigLM75BD(config->devAddr, config->osFaultQueueSize, config->osPolarity,
                                          config->osOperationMode, config->devOperationMode));
@@ -25,15 +28,29 @@ error_code_t lm75bdInit(lm75bd_config_t *config) {
   return ERR_CODE_SUCCESS;
 }
 
-error_code_t readTempLM75BD(uint8_t devAddr, float *temp) {
-  /* Implement this driver function */
-  
+error_code_t readTempLM75BD(uint8_t devAddr, float *temp)
+{
+  error_code_t errCode;
+  if (temp == NULL)
+  {
+    return ERR_CODE_INVALID_ARG;
+  }
+  // Write pointer data to select Temp Reg
+  uint8_t pointerData = TEMP_REG;
+  RETURN_IF_ERROR_CODE(i2cSendTo(devAddr, &pointerData, 1));
+  // Read temp data from Temp Reg
+  uint8_t tempData[2] = {0};
+  RETURN_IF_ERROR_CODE(i2cReceiveFrom(devAddr, tempData, 2));
+  // Convert temp to Celsius
+  int16_t rawTemp = (tempData[0] << 8) | tempData[1];
+  *temp = (float)(rawTemp >> 5) * 0.125;
   return ERR_CODE_SUCCESS;
 }
 
 #define CONF_WRITE_BUFF_SIZE 2U
 error_code_t writeConfigLM75BD(uint8_t devAddr, uint8_t osFaultQueueSize, uint8_t osPolarity,
-                                   uint8_t osOperationMode, uint8_t devOperationMode) {
+                               uint8_t osOperationMode, uint8_t devOperationMode)
+{
   error_code_t errCode;
 
   // Stores the register address and data to be written
@@ -44,21 +61,22 @@ error_code_t writeConfigLM75BD(uint8_t devAddr, uint8_t osFaultQueueSize, uint8_
   buff[0] = LM75BD_REG_CONF;
 
   uint8_t osFaltQueueRegData = 0;
-  switch (osFaultQueueSize) {
-    case 1:
-      osFaltQueueRegData = 0;
-      break;
-    case 2:
-      osFaltQueueRegData = 1;
-      break;
-    case 4:
-      osFaltQueueRegData = 2;
-      break;
-    case 6:
-      osFaltQueueRegData = 3;
-      break;
-    default:
-      return ERR_CODE_INVALID_ARG;
+  switch (osFaultQueueSize)
+  {
+  case 1:
+    osFaltQueueRegData = 0;
+    break;
+  case 2:
+    osFaltQueueRegData = 1;
+    break;
+  case 4:
+    osFaltQueueRegData = 2;
+    break;
+  case 6:
+    osFaltQueueRegData = 3;
+    break;
+  default:
+    return ERR_CODE_INVALID_ARG;
   }
 
   buff[1] |= (osFaltQueueRegData << 3);
@@ -67,7 +85,8 @@ error_code_t writeConfigLM75BD(uint8_t devAddr, uint8_t osFaultQueueSize, uint8_
   buff[1] |= devOperationMode;
 
   errCode = i2cSendTo(LM75BD_OBC_I2C_ADDR, buff, CONF_WRITE_BUFF_SIZE);
-  if (errCode != ERR_CODE_SUCCESS) return errCode;
+  if (errCode != ERR_CODE_SUCCESS)
+    return errCode;
 
   return ERR_CODE_SUCCESS;
 }
