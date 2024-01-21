3a80b0389ba0c8f1db44710e384316211bb5ba3d
loqoman <darwin.s.clark@gmail.com>
2024-01-21 11:24:28 -0800

    Working commit of overtemp interrupt and thermal manager implementation

--- services/thermal_mgr/thermal_mgr.c
+++ services/thermal_mgr/thermal_mgr.c
@@ -24,6 +24,8 @@ static uint8_t thermalMgrQueueStorageArea[THERMAL_MGR_QUEUE_LENGTH * THERMAL_MGR
 
 static void thermalMgr(void *pvParameters);
 
+static float cachedTemperatureReading = 0.0;  
+
 void initThermalSystemManager(lm75bd_config_t *config) {
   memset(&thermalMgrTaskBuffer, 0, sizeof(thermalMgrTaskBuffer));
   memset(thermalMgrTaskStack, 0, sizeof(thermalMgrTaskStack));
@@ -41,21 +43,73 @@ void initThermalSystemManager(lm75bd_config_t *config) {
 
 }
 
+/*
+ * @brief Sends event to the thermal manager queue. Fails is queue is full.
+ *
+ * @param event - Pointer to a thermal_mgr_event_t to add to the queue.
+ */
 error_code_t thermalMgrSendEvent(thermal_mgr_event_t *event) {
-  /* Send an event to the thermal manager queue */
+    /* Send an event to the thermal manager queue */
+    BaseType_t errorCode;
+    errorCode = xQueueSend( thermalMgrQueueHandle, (void *) event, 0 );
+    switch( errorCode ) {
+        case pdTRUE:
+            return ERR_CODE_SUCCESS; 
+            break;
+        case errQUEUE_FULL:
+            return ERR_CODE_QUEUE_FULL;    
+            break;
+        default:
+            return ERR_CODE_UNKNOWN;
+    }
+}
+/* 
+ * @brief Handles overtemperature events from temperature sensor. It is assumed a
+ *          read event will cause the interrupt to be called (I.E cachedTemperatureReading
+ *          is guarenteed to be updated)
+ */
+void osHandlerLM75BD() {     
+    if( cachedTemperatureReading > 80.0  ) {  
+        // We're over overtemp
+        overTemperatureDetected();  
+    } else {
+        // We're less than hysteresis  
+        safeOperatingConditions();
+    }
+    // Reset overtemperature   
+    thermal_mgr_event_t resetEvent;
+    resetEvent.type = THERMAL_MGR_EVENT_MEASURE_TEMP_CMD;  
 
-  return ERR_CODE_SUCCESS;
+    thermalMgrSendEvent( &resetEvent );   
 }
 
-void osHandlerLM75BD(void) {
-  /* Implement this function */
-}
+/*
+ * @brief Task responsible for parsing events in its queue 
+ *
+ * @param pvParameters - Configuration struct for the LM75BD temp sensor 
+ */
+static void thermalMgr(void *pvParameters) {      
+    // XXX: It is assumed we don't have to initialize temperature sensor
+    lm75bd_config_t *tempSensorConfig;  
+    error_code_t errCode;
+    tempSensorConfig = (lm75bd_config_t *) pvParameters;   
 
-static void thermalMgr(void *pvParameters) {
-  /* Implement this task */
-  while (1) {
-    
-  }
+    while (1) {  
+        thermal_mgr_event_t *event_recieved;
+        if ( xQueueReceive( thermalMgrQueueHandle, (void *) event_recieved, 0) == pdTRUE ) {    
+            // We got an event from the thermalMgr queue, parse it
+            switch( event_recieved->type ){  
+                case THERMAL_MGR_EVENT_MEASURE_TEMP_CMD:    
+                        // Read temperature 
+                        float temperatureResult = 0.0;   
+                        errCode = readTempLM75BD( tempSensorConfig->devAddr, &temperatureResult );  
+
+                        cachedTemperatureReading = temperatureResult;
+                        addTemperatureTelemetry( temperatureResult );
+                    break;  
+            }
+        }
+    }
 }
 
 void addTemperatureTelemetry(float tempC) {
