874ad6bdfc6bdb45d9db5825b41b7ec3777f2cdc
Cathouwu <62555400+Cathouwu@users.noreply.github.com>
2024-02-18 16:47:44 -0500

    Update lm75bd/lm75bd.c
    Co-authored-by: Daniel Gobalakrishnan <63618806+dgobalak@users.noreply.github.com>

--- lm75bd/lm75bd.c
+++ lm75bd/lm75bd.c
@@ -33,7 +33,7 @@ error_code_t readTempLM75BD(uint8_t devAddr, float *temp) {
   /* Implement this driver function */
   uint8_t tempReg[1]= {0};
   i2cSendTo(LM75BD_OBC_I2C_ADDR, tempReg, 1);
-  uint8_t tempData[2] = {0,0};
+  uint8_t tempData[2] = {0};
   i2cReceiveFrom(LM75BD_OBC_I2C_ADDR, tempData, 2);
   int16_t tempBin = ((tempData[0] << 8)|(tempData[1]))>>5;
   int16_t tempDec = 0;
