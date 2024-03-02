56b21a7aa81113ccf8c5da56b3688a8d667e66e5
AkshayaT1 <94016345+AkshayaT1@users.noreply.github.com>
2024-03-01 19:16:43 -0500

    Update lm75bd/lm75bd.c
    Co-authored-by: Daniel Gobalakrishnan <63618806+dgobalak@users.noreply.github.com>

--- lm75bd/lm75bd.c
+++ lm75bd/lm75bd.c
@@ -9,7 +9,7 @@
 
 /* LM75BD Registers (p.8) */
 #define LM75BD_REG_CONF 0x01U  /* Configuration Register (R/W) */
-#define LM75BD_TEMP_REG 0x00U
+#define LM75BD_REG_TEMP 0x00U
 
 error_code_t lm75bdInit(lm75bd_config_t *config) {
   error_code_t errCode;
