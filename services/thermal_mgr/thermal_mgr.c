147d1718e5a23f9513f8d82cf79dc0a71d36b77f
purekikan <kikansalavia@gmail.com>
2025-03-28 21:54:03 -0400

    fix OS handler error

--- services/thermal_mgr/thermal_mgr.c
+++ services/thermal_mgr/thermal_mgr.c
@@ -60,7 +60,7 @@ void osHandlerLM75BD(void) {
   uint8_t devAddr = LM75BD_OBC_I2C_ADDR;
   thermal_mgr_event_t event = {THERMAL_MGR_EVENT_HANDLE_OS};
   error_code_t errCode;
-  LOG_IF_ERROR_CODE(&event);
+  LOG_IF_ERROR_CODE(thermalMgrSendEvent(&event));
 }
 
 static void thermalMgr(void *pvParameters) {
