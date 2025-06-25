5c605910f661a216e569c4eb1cf975b154ff696a
Jack Dietrich <jed123@rogers.com>
2025-06-25 16:26:32 -0400

    LM75BD driver finished

--- lm75bd/lm75bd.c
+++ lm75bd/lm75bd.c
@@ -27,6 +27,7 @@ error_code_t lm75bdInit(lm75bd_config_t *config) {
 
 //This is probably not using pre-proc sparingly
 //removed as this was being re-defined and we only need to write the pointer byte, don't need a buffer
+#define WRITE_BUFF_SIZE 1U
 #define CONF_READ_BUFF_SIZE 2U
 #define LM75BD_PTR_BYTE 0x00U //should just be 0 as we want to read from the temp register. Thus this technically is not neccisary
 #define LM75BD_PTR_SEND 2U
@@ -44,14 +45,18 @@ error_code_t readTempLM75BD(uint8_t devAddr, float *temp) {
   
   */
 
+
+  uint8_t writeBuff[WRITE_BUFF_SIZE] = {0}; //for reading from LM75BD, should hold 2 bytes 
   uint8_t ReadBuff[CONF_READ_BUFF_SIZE] = {0}; //for reading from LM75BD, should hold 2 bytes 
 
-  uint8_t WriteBuff = LM75BD_PTR_BYTE;
+  
 
   ReadBuff[1] = LM75BD_PTR_BYTE;//Just 0 as we want to read
 
+  writeBuff[0] = LM75BD_PTR_BYTE;
 
-  errCode = i2cSendTo(LM75BD_OBC_I2C_ADDR, &WriteBuff, 1);//Send the pointer byte, the address should already be sent by the function itsself
+
+  errCode = i2cSendTo(LM75BD_OBC_I2C_ADDR, writeBuff, 1);//Send the pointer byte, the address should already be sent by the function itsself
   if(errCode != ERR_CODE_SUCCESS) return errCode;// if we failed
 
   //ack and restart should happen after this.
@@ -59,20 +64,28 @@ error_code_t readTempLM75BD(uint8_t devAddr, float *temp) {
   errCode = i2cReceiveFrom(LM75BD_OBC_I2C_ADDR, ReadBuff, LM75BD_TEMP_READ);//Recieve the temp data (this will be backwards as the device send MSB first)
   if(errCode != ERR_CODE_SUCCESS) return errCode;// if we failed
 
-
   //need to perform conversion on read in data(7.4.3), also msb in in index 0 and lsb is in index 1
 
-  uint16_t preTemp = ReadBuff[1] | (ReadBuff[0] << 8);//this is to put the MSB and LSB back into the correct places.
 
-  if((preTemp & (1 << 15)) == 1){//shift 1 left 15 bits to test d10 if it's a 1
+  uint16_t preTemp = ReadBuff[1] | (ReadBuff[0] << 8);//this is to put the MSB and LSB back into the correct places.
+  //preTemp = preTemp >> 5;//shift right by 5 as 5 lowest bits not being used
+  
+  float convTemp = 0;
+
+
+  if((preTemp & (1 << 15)) != 0){//shift 1 left 15 bits to test d10 if it's a 1 || We are never getting here
     //negative temp, take twos compliment
-    ~preTemp;//invert bits
-    preTemp+1;//add 1
+    preTemp = ~preTemp;//invert bits
+    preTemp++;//add 1 to pretemp
+    preTemp = preTemp >> 5;
+    convTemp = -(preTemp*0.125);//negate it
+  }else{
+    preTemp = preTemp >> 5;
+    convTemp = preTemp*0.125;
   }
 
-  float convTemp = preTemp*0.125;//need to retain numerical accuracy here. Temp is in ° celcius
 
-  temp = &convTemp;
+  *temp = convTemp;
   
 
   return ERR_CODE_SUCCESS;
