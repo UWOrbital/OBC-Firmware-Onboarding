ee12bff959b894004febbb7506521557592db2d2
Jack Dietrich <jed123@rogers.com>
2025-06-26 11:49:29 -0400

    Begun osHandler work

--- services/thermal_mgr/thermal_mgr.c
+++ services/thermal_mgr/thermal_mgr.c
@@ -53,6 +53,23 @@ error_code_t thermalMgrSendEvent(thermal_mgr_event_t *event) {//unsure how many 
 
 void osHandlerLM75BD(void) {
   /* Implement this function */
+
+  /* Notes
+  -Interrupts when going above Tth or below Thys. So we don't know weather its been called after going over Tth or below Thys.
+  -We must reset it by reading a register, but how to read a register without using the driver function??
+  -Appears that this is not beign called as I never see the printconsole message
+  */
+  printConsole("In Interrupt osHandler");
+
+  float temp = 0;
+  readTempLM75BD(LM75BD_OBC_I2C_ADDR,&temp);//will this block?
+
+  if( temp >  LM75BD_DEFAULT_HYST_THRESH){
+    overTemperatureDetected();
+  }else{
+    safeOperatingConditions();
+  }
+
 }
 
 static void thermalMgr(void *pvParameters) {
@@ -61,17 +78,16 @@ static void thermalMgr(void *pvParameters) {
   /* Notes
   -reveive event thru thermal manager queue 
   -check if it's type is set to THERMAL_MGR_EVENT_MEASURE_TEMP_CMD
-  -
+  -Not doing any error handling as no return value?
   */
   uint8_t i = 0;//for keeping track of where we are in the buffer.
   //create buffer to read into
   thermal_mgr_event_t buffer[THERMAL_MGR_QUEUE_LENGTH]; //create a buffer to read into, not sure how to initialize
 
-  while (1) {
-    if(xQueueReceive(thermalMgrQueueHandle, &buffer, 0) != pdTRUE){//
-    return errQUEUE_EMPTY;
-  }
-  //else we succesfully recieved an item into our buffer 
+  while (1) {//infinite loop that should be broken out of at some point
+  xQueueReceive(thermalMgrQueueHandle, &buffer, 0);//removed error checking
+
+
   if(buffer[i].type == THERMAL_MGR_EVENT_MEASURE_TEMP_CMD){//if the current element in the buffer is of the correct type
     float temp = 0; //initialize temp var
     readTempLM75BD(LM75BD_OBC_I2C_ADDR, &temp); //pass in the temp variable we just made.
@@ -82,6 +98,7 @@ static void thermalMgr(void *pvParameters) {
   if(i == THERMAL_MGR_QUEUE_LENGTH){
     i = 0;//reset buffer if it has reached the end
   }
+
 
   }
    
