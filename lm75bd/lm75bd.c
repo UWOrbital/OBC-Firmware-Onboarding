1ab3e73d4322dd5a42d0deeac56808c965e3d8ce
kg07 <kovidgramanan@icloud.com>
2025-07-10 21:58:23 -0400

    Attempt 1 at challenge

--- lm75bd/lm75bd.c
+++ lm75bd/lm75bd.c
@@ -8,12 +8,14 @@
 #include <math.h>
 
 /* LM75BD Registers (p.8) */
-#define LM75BD_REG_CONF 0x01U  /* Configuration Register (R/W) */
+#define LM75BD_REG_CONF 0x01U /* Configuration Register (R/W) */
 
-error_code_t lm75bdInit(lm75bd_config_t *config) {
+error_code_t lm75bdInit(lm75bd_config_t *config)
+{
   error_code_t errCode;
 
-  if (config == NULL) return ERR_CODE_INVALID_ARG;
+  if (config == NULL)
+    return ERR_CODE_INVALID_ARG;
 
   RETURN_IF_ERROR_CODE(writeConfigLM75BD(config->devAddr, config->osFaultQueueSize, config->osPolarity,
                                          config->osOperationMode, config->devOperationMode));
@@ -25,15 +27,26 @@ error_code_t lm75bdInit(lm75bd_config_t *config) {
   return ERR_CODE_SUCCESS;
 }
 
-error_code_t readTempLM75BD(uint8_t devAddr, float *temp) {
-  /* Implement this driver function */
+error_code_t readTempLM75BD(uint8_t devAddr, float *temp)
+{
+  if (temp == NULL) return ERR_CODE_INVALID_ARG;
+  error_code_t errCode;
+
+  uint8_t tempPointer = 0;
+  RETURN_IF_ERROR_CODE(i2cSendTo(devAddr, &tempPointer, sizeof(tempPointer)));
+
+  uint8_t tempData[2] = {0, 0};
+  RETURN_IF_ERROR_CODE(i2cReceiveFrom(devAddr, tempData, sizeof(tempData)));
+
+  *temp = (((int16_t)(tempData[0] << 8 | tempData[1])) >> 5) * 0.125f;
   
   return ERR_CODE_SUCCESS;
 }
 
 #define CONF_WRITE_BUFF_SIZE 2U
 error_code_t writeConfigLM75BD(uint8_t devAddr, uint8_t osFaultQueueSize, uint8_t osPolarity,
-                                   uint8_t osOperationMode, uint8_t devOperationMode) {
+                               uint8_t osOperationMode, uint8_t devOperationMode)
+{
   error_code_t errCode;
 
   // Stores the register address and data to be written
@@ -44,21 +57,22 @@ error_code_t writeConfigLM75BD(uint8_t devAddr, uint8_t osFaultQueueSize, uint8_
   buff[0] = LM75BD_REG_CONF;
 
   uint8_t osFaltQueueRegData = 0;
-  switch (osFaultQueueSize) {
-    case 1:
-      osFaltQueueRegData = 0;
-      break;
-    case 2:
-      osFaltQueueRegData = 1;
-      break;
-    case 4:
-      osFaltQueueRegData = 2;
-      break;
-    case 6:
-      osFaltQueueRegData = 3;
-      break;
-    default:
-      return ERR_CODE_INVALID_ARG;
+  switch (osFaultQueueSize)
+  {
+  case 1:
+    osFaltQueueRegData = 0;
+    break;
+  case 2:
+    osFaltQueueRegData = 1;
+    break;
+  case 4:
+    osFaltQueueRegData = 2;
+    break;
+  case 6:
+    osFaltQueueRegData = 3;
+    break;
+  default:
+    return ERR_CODE_INVALID_ARG;
   }
 
   buff[1] |= (osFaltQueueRegData << 3);
@@ -67,7 +81,8 @@ error_code_t writeConfigLM75BD(uint8_t devAddr, uint8_t osFaultQueueSize, uint8_
   buff[1] |= devOperationMode;
 
   errCode = i2cSendTo(LM75BD_OBC_I2C_ADDR, buff, CONF_WRITE_BUFF_SIZE);
-  if (errCode != ERR_CODE_SUCCESS) return errCode;
+  if (errCode != ERR_CODE_SUCCESS)
+    return errCode;
 
   return ERR_CODE_SUCCESS;
 }
--- services/thermal_mgr/thermal_mgr.c
+++ services/thermal_mgr/thermal_mgr.c
@@ -2,12 +2,14 @@
 #include "errors.h"
 #include "lm75bd.h"
 #include "console.h"
+#include "logging.h"
 
 #include <FreeRTOS.h>
 #include <os_task.h>
 #include <os_queue.h>
 
 #include <string.h>
+
 
 #define THERMAL_MGR_STACK_SIZE 256U
 
@@ -24,48 +26,105 @@ static uint8_t thermalMgrQueueStorageArea[THERMAL_MGR_QUEUE_LENGTH * THERMAL_MGR
 
 static void thermalMgr(void *pvParameters);
 
-void initThermalSystemManager(lm75bd_config_t *config) {
+void initThermalSystemManager(lm75bd_config_t *config)
+{
   memset(&thermalMgrTaskBuffer, 0, sizeof(thermalMgrTaskBuffer));
   memset(thermalMgrTaskStack, 0, sizeof(thermalMgrTaskStack));
-  
+
   thermalMgrTaskHandle = xTaskCreateStatic(
-    thermalMgr, "thermalMgr", THERMAL_MGR_STACK_SIZE,
-    config, 1, thermalMgrTaskStack, &thermalMgrTaskBuffer);
+      thermalMgr, "thermalMgr", THERMAL_MGR_STACK_SIZE,
+      config, 1, thermalMgrTaskStack, &thermalMgrTaskBuffer);
 
   memset(&thermalMgrQueueBuffer, 0, sizeof(thermalMgrQueueBuffer));
   memset(thermalMgrQueueStorageArea, 0, sizeof(thermalMgrQueueStorageArea));
 
   thermalMgrQueueHandle = xQueueCreateStatic(
-    THERMAL_MGR_QUEUE_LENGTH, THERMAL_MGR_QUEUE_ITEM_SIZE,
-    thermalMgrQueueStorageArea, &thermalMgrQueueBuffer);
-
+      THERMAL_MGR_QUEUE_LENGTH, THERMAL_MGR_QUEUE_ITEM_SIZE,
+      thermalMgrQueueStorageArea, &thermalMgrQueueBuffer);
 }
 
-error_code_t thermalMgrSendEvent(thermal_mgr_event_t *event) {
+error_code_t thermalMgrSendEvent(thermal_mgr_event_t *event)
+{
   /* Send an event to the thermal manager queue */
+  if (event == NULL || thermalMgrQueueHandle == NULL)
+    return ERR_CODE_INVALID_ARG;
+
+  if (xQueueSend(thermalMgrQueueHandle, event, 0) != pdPASS)
+  {
+    return ERR_CODE_QUEUE_FULL;
+  }
+
+  xQueueSend(thermalMgrQueueHandle, event, portMAX_DELAY);
 
   return ERR_CODE_SUCCESS;
 }
 
-void osHandlerLM75BD(void) {
+void osHandlerLM75BD(void)
+{
   /* Implement this function */
+  thermal_mgr_event_t event;
+  event.type = THERMAL_MGR_EVENT_MEASURE_TEMP_CMD;
+  thermalMgrSendEvent(&event);
 }
 
-static void thermalMgr(void *pvParameters) {
+static void thermalMgr(void *pvParameters)
+{
   /* Implement this task */
-  while (1) {
-    
+  lm75bd_config_t config = *(lm75bd_config_t *)pvParameters;
+  while (1)
+  {
+    thermal_mgr_event_t event;
+    if (xQueueReceive(thermalMgrQueueHandle, &event, portMAX_DELAY) != pdFAIL)
+    {
+      if (event.type == THERMAL_MGR_EVENT_MEASURE_TEMP_CMD)
+      {
+        float tempC;
+        error_code_t errCode = readTempLM75BD(config.devAddr, &tempC);
+        if (errCode != ERR_CODE_SUCCESS)
+        {
+          LOG_ERROR_CODE(errCode);
+          continue;
+        }
+        addTemperatureTelemetry(tempC);
+      }
+    }
+    else if (event.type == THERMAL_MGR_EVENT_MEASURE_TEMP_CMD)
+    {
+      float tempC;
+      error_code_t errCode = readTempLM75BD(config.devAddr, &tempC);
+      if (errCode != ERR_CODE_SUCCESS)
+      {
+        LOG_ERROR_CODE(errCode);
+        continue;
+      }
+
+      if (tempC > config.hysteresisThresholdCelsius)
+      {
+        overTemperatureDetected();
+      }
+      else
+      {
+        safeOperatingConditions();
+      }
+    }
+    else
+    {
+      LOG_ERROR_CODE(ERR_CODE_INVALID_QUEUE_MSG);
+    }
   }
 }
 
-void addTemperatureTelemetry(float tempC) {
+void addTemperatureTelemetry(float tempC)
+{
   printConsole("Temperature telemetry: %f deg C\n", tempC);
 }
 
-void overTemperatureDetected(void) {
+void overTemperatureDetected(void)
+{
   printConsole("Over temperature detected!\n");
 }
 
-void safeOperatingConditions(void) { 
+void safeOperatingConditions(void)
+{
   printConsole("Returned to safe operating conditions!\n");
 }
