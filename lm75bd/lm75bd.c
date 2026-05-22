52ab6a12310d7596247e45cb03fc27c28d5a9695
array3000 <a28ray@uwaterloo.ca>
2026-05-22 17:56:03 -0400

    Finished the assignment, tested on unit and integration and works

--- lm75bd/lm75bd.c
+++ lm75bd/lm75bd.c
@@ -27,7 +27,39 @@ error_code_t lm75bdInit(lm75bd_config_t *config) {
 
 error_code_t readTempLM75BD(uint8_t devAddr, float *temp) {
   /* Implement this driver function */
-  
+  // need to access pointer register 000000 (00h)(temp register)
+  // 4 bytes = 16 bits
+  //uint8_t used b/c I2C works in 8 bit sequences
+  uint8_t i2cBuffer[2];
+  uint8_t pointerBuf = 0; // this should tell the pointer register to send from temperature register
+  // tell the slave sensor via i2c you want to get data (idk how many bytes are needed for i2c handshake)
+  i2cSendTo(devAddr, &pointerBuf, 1);
+
+  //should read the 2 bytes (16 bits) form the temperature register, and ignore the last 5 (this function handles stop condition)
+  i2cReceiveFrom(devAddr, i2cBuffer, 2);
+  // now i2cBuffer[0] has first 8 bits, and [1] has last 8 bits
+  // in a uint16_t, shift the high byte up by 8 bits since its the first 8 bits and attach the last 8 with bitwise "or"
+  uint16_t temperature_binary = i2cBuffer[0] << 8 | i2cBuffer[1];
+  // To filter out, you know the last 5 are 0/useless, so just shift all the 11 valuable bits to the left to get the value you want!!
+  // Check the sign of the MSB to see if its +ve or -ve
+
+  // bit mask with MSB to see if negative bit set or not
+  if (temperature_binary & (1 << 15)){
+    // then right shift to filter out unneeded bits at bottom
+    temperature_binary = temperature_binary >> 5;
+    // TODO: now sign extend, why again?
+    temperature_binary |= 0xF800; // bitwise OR with 1111 1000 0000 0000, extending negative sign
+    //(from datasheet) since negative, C = -(twos compliment of temp data) * 0.125
+    *temp = (int16_t)temperature_binary * 0.125; 
+    
+  }
+  else{
+    // if its zero
+    temperature_binary = temperature_binary >> 5;
+    // since positive, C = Tempdata * 0.125C
+    *temp = temperature_binary * 0.125;
+  }
+
   return ERR_CODE_SUCCESS;
 }
 
--- services/thermal_mgr/thermal_mgr.c
+++ services/thermal_mgr/thermal_mgr.c
@@ -43,18 +43,61 @@ void initThermalSystemManager(lm75bd_config_t *config) {
 
 error_code_t thermalMgrSendEvent(thermal_mgr_event_t *event) {
   /* Send an event to the thermal manager queue */
-
-  return ERR_CODE_SUCCESS;
+if (xQueueSend(thermalMgrQueueHandle, event, pdMS_TO_TICKS(100)) == pdPASS)
+    return ERR_CODE_SUCCESS;
+  return ERR_CODE_QUEUE_FULL;
 }
 
 void osHandlerLM75BD(void) {
   /* Implement this function */
+  // do you also need to take the config to send the overtemperature threshold as well?
+  // read temperature or put sensor in shutdown morde to reset the OS output once its active (what is the OS output?)
+  float temperature = 0;
+  // since ISR, how do I read the devAddr? use define
+  readTempLM75BD(LM75BD_OBC_I2C_ADDR, &temperature); 
+  // better to call overTemperatureDetected and safeOperatingConditions functions in thermalManagement task
+  // GET t_hys from where??? hardcode
+  // THis is the hysteresis temeprature
+  if (temperature > 75.0f){
+    // ideally take out of ISR
+    overTemperatureDetected();
+  }
+  else{
+    // ideally take out of ISR
+    safeOperatingConditions();
+  }
+  // call overTempeartureDetected if you exceeded T_th and call safe OperatingConditions when we're dropped back down below T_hys
+  // This is an ISR  function, so keep as small as possible
+
 }
 
 static void thermalMgr(void *pvParameters) {
   /* Implement this task */
+  // get the devAddr directly from the sensor config struct pointer, which is passed from pvParameters, set when the task is created?
+  lm75bd_config_t config = *(lm75bd_config_t *) pvParameters;
+  config.hysteresisThresholdCelsius;  // cache it before ISR can fire
+  thermal_mgr_event_t event; 
+
   while (1) {
-    
+    if (xQueueReceive(thermalMgrQueueHandle, &event, pdMS_TO_TICKS(5)) == pdPASS) {
+            // Handle event
+            float temperature = 0;
+            switch (event.type) {
+                case THERMAL_MGR_EVENT_MEASURE_TEMP_CMD:
+                    // measure 
+                    readTempLM75BD(config.devAddr, &temperature); 
+                    // send as telemetry
+                    addTemperatureTelemetry(temperature);
+                    break;
+                case THERMAL_MGR_EVENT_FAULT:
+                    // measure 
+                    readTempLM75BD(config.devAddr, &temperature); 
+                    addTemperatureTelemetry(temperature);
+                    overTemperatureDetected();
+                default:
+                    break;
+      }
+    }
   }
 }
 
@@ -69,3 +112,4 @@ void overTemperatureDetected(void) {
 void safeOperatingConditions(void) { 
   printConsole("Returned to safe operating conditions!\n");
 }
+
